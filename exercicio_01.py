d6c2c2ef1f297eeb0d7f07b35917dd76a37b2d0d
irven-xavier <irven.xavier@gmail.com>
2025-10-28 10:17:01 -0300

    resolvendo exercicio 01

---
+++ exercicio_01.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def filtrar_acima_de(valores: List[float], limite: float) -> List[float]:
+    
+    resultado = []
+    
+    for valor in valores:
+        if valor > limite:
+            resultado.append(valor)
+
+    return resultado
+
+if __name__ == "__main__":
+
+    lista_de_valores = [10, 10.2, 1000, 1908, 178.02]
+
+    resultados = filtrar_acima_de(lista_de_valores, 999)
+
+    print(resultados)
